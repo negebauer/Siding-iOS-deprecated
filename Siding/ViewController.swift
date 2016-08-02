42fa3424408cccb2ac7962121babe3638c155b19
negebauer <negebauer@uc.cl>
2016-08-01 22:46:23 -0400

    Initial Commit

---
+++ Siding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Siding
+//
+//  Created by Nicolás Gebauer on 01-08-16.
+//  Copyright © 2016 Nicolás Gebauer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
